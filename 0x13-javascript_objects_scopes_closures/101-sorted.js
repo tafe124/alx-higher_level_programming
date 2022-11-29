179b542f6d358ef4388f284e70878d2720d3f7b5
tafe124 <tafesegechera22@gmail.com>
2022-11-29 19:31:41 +0000

    done

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const dict = require('./101-data.js').dict;
+
+const newDict = {};
+for (const key in dict) {
+  const value = dict[key];
+  if (newDict[value] === undefined) {
+    newDict[value] = [key];
+  } else {
+    newDict[value].push(key);
+  }
+}
+console.log(newDict);
