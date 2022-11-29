94f0142d60a529bb0678fb644e9643495116491e
tafe124 <tafesegechera22@gmail.com>
2022-11-29 14:07:34 +0000

    done

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+ass Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let row = '';
+    for (let i = 0; i < this.width; i++) {
+      row += 'X';
+    }
+    for (let j = 0; j < this.height; j++) {
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Rectangle;
