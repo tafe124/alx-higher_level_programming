7d398e0a2904e4afa47c3db297de88e2a269c938
tafe124 <tafesegechera22@gmail.com>
2022-12-02 21:50:47 +0000

    done

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
-""" script that prints all the City objects """
+"""
+List all State objects
+"""
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import Base, State
+    from model_city import City
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
 
+    target = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine(target.format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State, City).filter(State.id == City.state_id)
+    for s, c in states.order_by(City.id).all():
+        print("{:s}: ({:d}) {:s}".format(s.name, c.id, c.name))
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-import sys
-from model_state import Base, State
-from model_city import City
-
-
-if __name__ == '__main__':
-    h = 'localhost'
-    u = sys.argv[1]
-    pas = sys.argv[2]
-    db_n = sys.argv[3]
-    p = 3306
-    eng = create_engine('mysql+mysqldb://{}:{}@{}:{}/{}'.format(
-          u, pas, h, p, db_n))
-
-    Session = sessionmaker(bind=eng)
-    r = Session().query(City, State).join(State)
-
-    for city, state in r:
-        print("{}: ({})  {}".format(state.name, city.id, city.name))
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
-""" contains the class definition of a City and an instance of Base """
-
-from sqlalchemy import Column, Integer, String
+"""
+Creating a City object that inherits from Base
+"""
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 from model_state import Base, State
-Base = declarative_base()
 
 
 class City(Base):
-    """ defines City class """
+    """
+    Creating a City object that inherits from Base
+    """
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False, unique=True,
-        autoincrement=True)
-    name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey(States.id), nullable=False)
+
+    id = Column(Integer,
+                primary_key=True,
+                autoincrement=True,
+                nullable=False)
+    name = Column(String(128),
+                  nullable=False)
+    state_id = Column(Integer,
+                      ForeignKey("states.id"),
+                      nullable=False)
