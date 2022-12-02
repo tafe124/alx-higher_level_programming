dc1f117bce5f3c816e554c8eb365f3d2d33b7f32
tafe124 <tafesegechera22@gmail.com>
2022-12-02 22:02:37 +0000

    done

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
 """
-Creating a City object that inherits from Base
+This script defines a City class
+to work with MySQLAlchemy ORM.
 """
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship
+
 from model_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 class City(Base):
-    """
-    Creating a City object that inherits from Base
+    """City class
+    Attributes:
+        __tablename__ (str): The table name of the class
+        id (int): The id of the class
+        name (str): The name of the class
+        state_id (int): The state the city belongs to
     """
     __tablename__ = 'cities'
 
-    id = Column(Integer,
-                primary_key=True,
-                autoincrement=True,
-                nullable=False)
-    name = Column(String(128),
-                  nullable=False)
-    state_id = Column(Integer,
-                      ForeignKey("states.id"),
-                      nullable=False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
