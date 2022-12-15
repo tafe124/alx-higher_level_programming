10c4cad31e052561447b41cf7e1b54620113986a
tafe124 <tafesegechera22@gmail.com>
2022-12-15 21:43:58 +0000

    done

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
-# script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -s -d "email=test@gmail.com&subject=I will always be here for PLD" -H "Content-Type: application/x-www-form-urlencoded" -X POST "$1"
+!/bin/bash
+# Bash script that sends a POST request and displays the body of the response
+curl -X POST "$1" -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
 
