dd61a0b17fef253ff29383dfc506644b5bf5fed8
tafe124 <tafesegechera22@gmail.com>
2022-12-16 23:43:04 +0000

    done

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""
-Python script that list 10 commits (from the most recent to oldest) of the repository
+"""List 10 commits (from the most recent to oldest)
+of a repository by user.
+This script use the Github API and print all commits
+by: `<sha>: <author name>` (one by line).
 """
 
-import requests
 from sys import argv
+import requests
+
 
 if __name__ == "__main__":
-    repo = argv[1]
-    owner = argv[2]
-    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
-    res = requests.get(url)
-    commits = res.json()
-    try:
-        for i in range(10):
-            sha = commits[i].get('sha')
-            author = commits[i].get('commit').get('author').get('name')
-            print("{}: {}".format(sha, author))
-    except IndexError:
-        pass
+    url = 'https://api.github.com'
+    uri = '{0}/repos/{1}/{2}/commits'.format(url, argv[2], argv[1])
+    req = requests.get(uri).json()
+
+    for com in req[0:10]:
+        print(com['sha'] + ':', com['commit']['author']['name'])
