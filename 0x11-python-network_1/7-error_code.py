99b198c8a4b3b22265e8aca56cb3c4b15c1f3dd6
tafe124 <tafesegechera22@gmail.com>
2022-12-16 23:55:32 +0000

    redone

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""Takes in a URL, sends a request to the URL and
-displays the body of the response (decoded in utf-8)
-with `requests` module.
 """
+a Python script that takes in a URL,
+sends a request to the URL and displays the body of the response.
+"""
+
 
 from sys import argv
 import requests
 
 
-if __name__ == "__main__":
-    req = requests.get(argv[1])
-
-    if req.status_code >= 400:
-        print('Error code:', req.status_code)
+if __name__ == '__main__':
+    r = requests.get(argv[1])
+    if r.status_code < 400:
+        print(r.text)
     else:
-        print(req.text)
+        print("Error code: {}".format(r.status_code))
