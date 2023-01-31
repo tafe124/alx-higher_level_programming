2a47dd56d3bc4e77f506d7e0bea1d73c68c2c76f
tafe124 <tafesegechera22@gmail.com>
2023-01-31 20:33:11 +0000

    done

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const request = require('request');
+const argv = process.argv;
+
+const baseUrl = 'https://swapi-api.hbtn.io/api/films/';
+const url = baseUrl + argv[2];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const title = JSON.parse(body).title;
+  console.log(title);
+})i;
